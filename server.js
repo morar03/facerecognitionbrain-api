9aa4b1e3ec4a5b4a4058fad01955227e30279504
morar03 <morarcristiantraian@gmail.com>
2021-03-10 20:00:44 +0200

    add port

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ bcrypt.compare("veggies", hash, function(err, res) {
 });
 */
 
-app.listen(3001, ()=>{
-	console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3001, ()=>{
+	console.log(`app is running on port ${ process.env.PORT}`);
 })
 
