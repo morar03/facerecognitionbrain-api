768a58f6fc2fef8d5fd3a61f97fef947779d87df
morar03 <morarcristiantraian@gmail.com>
2021-03-10 19:31:46 +0200

    commit

---
+++ controllers/profile.js
@@ -0,0 +1,17 @@
+ 
+ const handleProfile = (req, res) =>{
+	const { id } = req.params;
+	db.select('*').from('users').where({id})
+	.then(user => {
+		if (user.length){
+			res.json(user[0])
+		}else{
+			res.status(400).json('Not Found')
+		}
+	})
+	.catch(err => res.status(400).json('errorr getting user'))
+}
+
+module.exports = {
+	handleProfile: handleProfile
+};
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const app = express();
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'admin',
+		database: 'smart-brain'
+	}
+});
+
+
+
+app.use(express.json());
+app.use(cors());
+
+
+/*	/ --> res = this is working	*/
+app.get('/', (req, res)=>{
+	res.send('succes');
+})
+
+/*	/signin --> POST  = sicces/fail	*/
+app.post('/signin', (req, res) => {sigin.handleSignIn(req, res, db, bcrypt)})
+
+/*  /register --> POST = user */
+app.post('/register', (req,res) => {register.handleRegister(req, res, db, bcrypt)})
+
+/*	/profile/:userId --> GET = user 	*/
+
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res)})
+
+
+/*	/image --> PUT --> user 	*/
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+
+/*// Load hash from your password DB.
+bcrypt.compare("bacon", hash, function(err, res) {
+    // res == true
+});
+bcrypt.compare("veggies", hash, function(err, res) {
+    // res = false
+});
+*/
+
+app.listen(3001, ()=>{
+	console.log('app is running on port 3001');
+})
+
