b0c05bbf7337e9ed5fd28ffe091bd81088326af3
morar03 <morarcristiantraian@gmail.com>
2021-03-11 13:50:57 +0200

    update

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: '6cbb38e1191745e5bddfc44663e3b749'
+ apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
